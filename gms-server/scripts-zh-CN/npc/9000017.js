7c838e2e87e54d29c9ff0fae4ebf028258b68f4b
maplepeng <fengyi_pc@foxmail.com>
2025-04-12 15:57:06 +0800

    汉化: 嘉年华&可可 脚本汉化

--- gms-server/scripts-zh-CN/npc/9000017.js
+++ gms-server/scripts-zh-CN/npc/9000017.js
@@ -17,11 +17,11 @@
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-/* Coco
-        Refining NPC: 
-	* Chaos scroll SYNTHETIZER (rofl)
-        * 
-        * @author RonanLana (ronancpl)
+/* 可可
+        精炼NPC：
+	* 混沌卷轴合成器（笑）
+        *
+        * @作者 RonanLana (ronancpl)
 */
 
 var status = 0;
@@ -33,7 +33,7 @@ var matQty;
 var cost;
 var qty;
 var equip;
-var last_use; //last item is a use item
+var last_use; //最后一项是消耗品
 
 function start() {
     cm.getPlayer().setCS(true);
@@ -45,7 +45,7 @@ function action(mode, type, selection) {
     if (mode == 1) {
         status++;
     } else {
-        cm.sendOk("哦，好吧... 你想做生意的时候再来找我们吧。");
+        cm.sendOk("哦，好吧...想做生意的时候再来找我们吧。");
         cm.dispose();
         return;
     }
@@ -53,15 +53,15 @@ function action(mode, type, selection) {
     if (status == 0) {
         const GameConfig = Java.type('org.gms.config.GameConfig');
         if (!GameConfig.getServerBoolean("use_enable_custom_npc_script")) {
-            cm.sendOk("嗨，我是 #b#p" + cm.getNpc() + "##k。");
+            cm.sendOk("嗨，我是#b#p" + cm.getNpc() + "##k。");
             cm.dispose();
             return;
         }
 
-        var selStr = "Hey traveler! Come, come closer... We offer a #bhuge opportunity of business#k to you. If you want to know what it is, keep listening...";
+        var selStr = "嘿，冒险家！过来，靠近点...我们有个#b绝佳的商机#k要告诉你。想知道是什么吗？继续听下去...";
         cm.sendNext(selStr);
     } else if (status == 1) {
-        var selStr = "We've got here the knowledge to synthetize the mighty #b#t2049100##k! Of course, making one is not an easy task... But worry not! Just gather some material to me and a fee of #b1,200,000 mesos#k for our services to #bobtain it#k. You still want to do it?";
+        var selStr = "我们掌握了合成强大的#b#t2049100##k的技艺！当然，制作它并不容易...但别担心！只要收集一些材料给我，并支付#b1,200,000金币#k的服务费，就能#b获得它#k。你还想继续吗？";
         cm.sendYesNo(selStr);
     } else if (status == 2) {
         //selectedItem = selection;
@@ -76,40 +76,40 @@ function action(mode, type, selection) {
         matQty = matQtySet[selectedItem];
         cost = costSet[selectedItem];
 
-        var prompt = "So, you want us to make some #t" + item + "#? In that case, how many do you want us to make?";
+        var prompt = "那么，你想让我们制作一些#t" + item + "#？这样的话，你想让我们制作多少呢？";
         cm.sendGetNumber(prompt, 1, 1, 100)
     } else if (status == 3) {
         qty = (selection > 0) ? selection : (selection < 0 ? -selection : 1);
         last_use = false;
 
-        var prompt = "You want us to make ";
+        var prompt = "你想让我们制作";
         if (qty == 1) {
-            prompt += "a #t" + item + "#?";
+            prompt += "一个#t" + item + "#？";
         } else {
-            prompt += qty + " #t" + item + "#?";
+            prompt += qty + "个#t" + item + "#？";
         }
 
-        prompt += " In that case, we're going to need specific items from you in order to make it. Make sure you have room in your inventory, though!#b";
+        prompt += "这样的话，我们需要你提供特定的材料。记得确保你的背包有足够的空间！#b";
 
         if (mats instanceof Array) {
             for (var i = 0; i < mats.length; i++) {
-                prompt += "\r\n#i" + mats[i] + "# " + matQty[i] * qty + " #t" + mats[i] + "#";
+                prompt += "\r\n#i" + mats[i] + "# " + matQty[i] * qty + "个#t" + mats[i] + "#";
             }
         } else {
-            prompt += "\r\n#i" + mats + "# " + matQty * qty + " #t" + mats + "#";
+            prompt += "\r\n#i" + mats + "# " + matQty * qty + "个#t" + mats + "#";
         }
 
         if (cost > 0) {
-            prompt += "\r\n#i4031138# " + cost * qty + " meso";
+            prompt += "\r\n#i4031138# " + cost * qty + "金币";
         }
         cm.sendYesNo(prompt);
     } else if (status == 4) {
         var complete = true;
 
         if (cm.getMeso() < cost * qty) {
-            cm.sendOk("快点！我们不是来帮你的忙的！我们都需要钱来过好生活，所以拿钱来让我们做成交并开始合成。");
+            cm.sendOk("得了吧！我们可不是在做慈善！大家都需要钱过日子，带上钱来我们才能开始交易和合成。");
         } else if (!cm.canHold(item, qty)) {
-            cm.sendOk("在我们交易之前，你没有检查一下你的背包里是否有空位吗？");
+            cm.sendOk("你都没检查背包有没有空间就来谈生意了？");
         } else {
             if (mats instanceof Array) {
                 for (var i = 0; complete && i < mats.length; i++) {
@@ -124,7 +124,7 @@ function action(mode, type, selection) {
             }
 
             if (!complete) {
-                cm.sendOk("你在开玩笑吧？没有所有的材料在手，我们将无法启动这个过程。去把它们都拿来，然后再来找我们谈！");
+                cm.sendOk("你在开玩笑吧？没有所有材料我们可没法开始制作。去收集齐了再来找我们！");
             } else {
                 if (mats instanceof Array) {
                     for (var i = 0; i < mats.length; i++) {
@@ -135,7 +135,7 @@ function action(mode, type, selection) {
                 }
                 cm.gainMeso(-cost * qty);
                 cm.gainItem(item, qty);
-                cm.sendOk("哇... 真不敢相信它起作用了！想想一会儿它竟然能够... 咳咳。当然它起作用了，我们所有的工作都非常高效！很高兴和你做生意。");
+                cm.sendOk("哇...真不敢相信成功了！刚才还以为会失...咳咳。当然会成功，我们的工作一向高效！和你做生意很愉快。");
             }
         }
         cm.dispose();
