133d702cec0f0870331c4b9cb014191c4971d6db
maplepeng <fengyi_pc@foxmail.com>
2025-04-15 23:15:09 +0800

    feat: 调整饰品制作要求，暂时不开放腰带&勋章制作

--- gms-server/scripts-zh-CN/npc/9000036.js
+++ gms-server/scripts-zh-CN/npc/9000036.js
@@ -48,7 +48,7 @@ function start() {
 
     cm.getPlayer().setCS(true);
     var selStr = "你好，我是#b饰品制作大师#k！我的作品以精良著称，不仅能完美复刻外观，连属性都能完全重现！只需要一些制作材料和少许服务费。您对哪种装备感兴趣呢？#b";
-    var options = ["项链", "脸部装饰", "眼镜", "腰带和勋章", "戒指"/*,"#t4032496#"*/];
+    var options = ["项链", "脸部装饰", "眼镜"/*, "腰带和勋章"*/, "戒指"/*,"#t4032496#"*/];
     for (var i = 0; i < options.length; i++) {
         selStr += "\r\n#L" + i + "# " + options[i] + "#l";
     }
@@ -66,48 +66,48 @@ function action(mode, type, selection) {
             var selStr = "这些是我能制作的项链：#b";
             items = [1122018, 1122007, 1122001, 1122003, 1122004, 1122006, 1122002, 1122005, 1122058];
             for (var i = 0; i < items.length; i++) {
-                selStr += "\r\n#L" + i + "##t" + items[i] + "##b";
+                selStr += "\r\n#L" + i + "##z" + items[i] + "##b";
             }
         } else if (selection == 1) { //face accessory
             var selStr = "脸部装饰？这些我可以制作：#b";
             items = [1012181, 1012182, 1012183, 1012184, 1012185, 1012186, 1012108, 1012109, 1012110, 1012111];
             for (var i = 0; i < items.length; i++) {
-                selStr += "\r\n#L" + i + "##t" + items[i] + "##b";
+                selStr += "\r\n#L" + i + "##z" + items[i] + "##b";
             }
         } else if (selection == 2) { //eye accessory
             var selStr = "需要眼镜吗？想让我制作哪种款式？#b";
-            items = [1022073, 1022088, 1022103, 1022089, 1022082];
+            items = [1022088, 1022103, 1022089];
             for (var i = 0; i < items.length; i++) {
-                selStr += "\r\n#L" + i + "##t" + items[i] + "##b";
+                selStr += "\r\n#L" + i + "##z" + items[i] + "##b";
             }
         } else if (selection == 3) { //belt & medal
-            var selStr = "嗯...这些有点特殊。因为这类物品尺寸较小且相似度很高，我无法保证最终会合成出什么。还想尝试吗？";
-            items = [];
-            maxEqp = 0;
-
-            for (var x = 1132005; x < 1132017; maxEqp++, x++) {
-                items[maxEqp] = x;
-            }
-
-            for (var x = 1142000; x < 1142102; maxEqp++, x++) {
-                items[maxEqp] = x;
-            }
-
-            for (var x = 1142107; x < 1142121; maxEqp++, x++) {
-                items[maxEqp] = x;
-            }
-
-            for (var x = 1142122; x < 1142143; maxEqp++, x++) {
-                items[maxEqp] = x;
-            }
-            selStr += "\r\n#L" + i + "##b尝试制作！#b";
-
-        } else if (selection == 4) { //ring refine
+//            var selStr = "嗯...这些有点特殊。因为这类物品尺寸较小且相似度很高，我无法保证最终会合成出什么。还想尝试吗？";
+//            items = [];
+//            maxEqp = 0;
+//
+//            for (var x = 1132005; x < 1132017; maxEqp++, x++) {
+//                items[maxEqp] = x;
+//            }
+//
+//            for (var x = 1142000; x < 1142102; maxEqp++, x++) {
+//                items[maxEqp] = x;
+//            }
+//
+//            for (var x = 1142107; x < 1142121; maxEqp++, x++) {
+//                items[maxEqp] = x;
+//            }
+//
+//            for (var x = 1142122; x < 1142143; maxEqp++, x++) {
+//                items[maxEqp] = x;
+//            }
+//            selStr += "\r\n#L" + i + "##b尝试制作！#b";
+//
+//        } else if (selection == 4) { //ring refine
             var selStr = "戒指？这可是我的拿手好戏，来看看吧！#b";
-            items = [1112407, 1112408, 1112401, 1112413, 1112414, 1112405, 1112402];
+            items = [1112407, 1112408, 1112401, 1112402, 1112413, 1112414, 1112405];
 
             for (var i = 0; i < items.length; i++) {
-                selStr += "\r\n#L" + i + "##t" + items[i] + "##b";
+                selStr += "\r\n#L" + i + "##z" + items[i] + "##b";
             }
 
         }/*else if (selection == 5) { //make necklace
@@ -119,59 +119,59 @@ function action(mode, type, selection) {
         selectedType = selection;
         cm.sendSimple(selStr);
     } else if (status == 1) {
-        if (selectedType != 3) {
+//        if (selectedType != 3) {
             selectedItem = selection;
-        }
+//        }
 
         if (selectedType == 0) { //pendant refine
             var matSet = [[4003004, 4030012, 4001356, 4000026], [4000026, 4001356, 4000073, 4001006], [4001343, 4011002, 4003004, 4003005], [4001343, 4011006, 4003004, 4003005], [4000091, 4011005, 4003004, 4003005], [4000091, 4011001, 4003004, 4003005], [4000469, 4011000, 4003004, 4003005], [4000469, 4011004, 4003004, 4003005], [1122007, 4003002, 4000413]];
-            var matQtySet = [[20, 20, 5, 1], [5, 5, 10, 1], [10, 2, 20, 4], [10, 1, 20, 4], [15, 3, 30, 6], [15, 3, 30, 6], [20, 5, 20, 8], [20, 4, 40, 8], [1, 1, 1]];
-            var costSet = [150000, 500000, 200000, 200000, 300000, 300000, 400000, 400000, 2500000];
+            var matQtySet = [[100,100,25, 100], [200, 50, 200, 10], [100, 5, 100, 50], [100, 3, 100, 50], [100, 6, 150, 75], [100, 6, 150, 75], [200, 10, 100, 100], [200, 8, 200, 100], [1, 1, 1]];
+            var costSet = [1000000, 1500000, 1000000, 1000000, 1500000, 1500000, 2000000, 2000000, 5000000];
         } else if (selectedType == 1) { //face accessory refine
             var matSet = [[4006000, 4003004], [4006000, 4003004, 4000026], [4006000, 4003004, 4000026, 4000082, 4003002], [4006000, 4003005], [4006000, 4003005, 4000026], [4006000, 4003005, 4000026, 4000082, 4003002], [4001006, 4011008], [4001006, 4011008], [4001006, 4011008], [4001006, 4011008]];
-            var matQtySet = [[5, 5], [5, 5, 5], [5, 5, 5, 5, 1], [5, 5], [5, 5, 5], [5, 5, 5, 5, 1], [1, 1], [1, 1], [1, 1], [1, 1]];
-            var costSet = [100000, 200000, 300000, 125000, 250000, 375000, 500000, 500000, 500000, 500000, 25000, 25000, 25000, 25000];
-        } else if (selectedType == 2) { //eye accessory refine
-            var matSet = [[4001006, 4003002, 4000082, 4031203], [4001005, 4011008], [4001005, 4011008], [4001005, 4011008, 4000082], [4001006, 4003002, 4003000, 4003001]];
-            var matQtySet = [[2, 2, 5, 10], [3, 2], [4, 3], [5, 3, 10], [2, 2, 10, 5]];
-            var costSet = [250000, 250000, 300000, 400000, 200000];
+            var matQtySet = [[20, 20], [20, 20, 20], [20, 20, 20, 15, 3], [20, 20], [20, 20, 20], [20, 20, 20, 15, 3], [10, 3], [10, 3], [10, 3], [10, 3]];
+            var costSet = [500000, 1000000, 1500000, 500000, 100000, 1500000, 2000000, 2000000, 2000000, 2000000];
+        } else if (selectedType == 2) { //eye accessory refine 1022088, 1022103
+            var matSet = [[4001005, 4011008], [1022088, 4001005, 4011008], [1022103, 4001005, 4011008, 4000082]];
+            var matQtySet = [[3, 2], [1, 4, 3], [1, 5, 3, 10]];
+            var costSet = [1500000, 2000000, 3000000];
         } else if (selectedType == 3) { //belt & medals refine
-            var matSet = [[4001006, 4003005, 4003004], [7777, 7777]];
-            var matQtySet = [[2, 5, 10], [7777, 7777]];
-            var costSet = [15000, 7777];
-        } else if (selectedType == 4) { //ring refine
-            var matSet = [[4003001, 4001344, 4006000], [4003001, 4001344, 4006000], [4021004, 4011008], [4011008, 4001006], [1112413, 2022039], [1112414, 4000176], [4011007, 4021009]];
-            var matQtySet = [[2, 2, 2], [2, 2, 2], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1]];
-            var costSet = [10000, 10000, 10000, 20000, 15000, 15000, 10000];
+//            var matSet = [[4001006, 4003005, 4003004], [7777, 7777]];
+//            var matQtySet = [[2, 5, 10], [7777, 7777]];
+//            var costSet = [15000, 7777];
+//        } else if (selectedType == 4) { //ring refine
+            var matSet = [[4003001, 4001344, 4006000], [4003001, 4001344, 4006000], [4021004, 4011008], [4011007, 4021009], [4011008, 4001006], [1112413, 2022039], [1112414, 4000176]];
+            var matQtySet = [[50, 50, 20], [50, 50, 20], [1, 1], [1, 1], [3, 10], [1, 10], [1, 3]];
+            var costSet = [500000, 500000, 1000000, 1000000, 1500000, 3000000, 5000000];
         }/*else if (selectedType == 5) { //necklace refine
             var matSet = [[4011007, 4011008, 4021009]];
             var matQtySet = [[1, 1, 1]];
             var costSet = [10000];
         }*/
 
-        if (selectedType == 3) {
-            selectedItem = Math.floor(Math.random() * maxEqp);
-            item = items[selectedItem];
-            mats = matSet[0];
-            matQty = matQtySet[0];
-            cost = costSet[0];
-        } else {
+//        if (selectedType == 3) {
+//            selectedItem = Math.floor(Math.random() * maxEqp);
+//            item = items[selectedItem];
+//            mats = matSet[0];
+//            matQty = matQtySet[0];
+//            cost = costSet[0];
+//        } else {
             item = items[selectedItem];
             mats = matSet[selectedItem];
             matQty = matQtySet[selectedItem];
             cost = costSet[selectedItem];
-        }
+//        }
 
         var prompt = "您想让我制作";
-        if (selectedType != 3) {
+//        } else if (selectedType != 3) {
             if (qty == 1) {
                 prompt += "一件#b#t" + item + "##k吗？";
             } else {
                 prompt += "#b" + qty + "件#t" + item + "##k吗？";
             }
-        } else {
-            prompt += "一条#b腰带#k或一枚#b勋章#k吗？";
-        }
+//        } else {
+//            prompt += "一条#b腰带#k或一枚#b勋章#k吗？";
+//        }
 
         prompt += " 好的！制作需要以下材料。请确保背包有#b空位#k！#b";
         if (mats instanceof Array) {
