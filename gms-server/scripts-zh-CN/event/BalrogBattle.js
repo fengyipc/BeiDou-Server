3d141a5660bdcffa82da78a1d1618cd5b61e1e59
maplepeng <maplepeng@tencent.com>
2026-04-19 18:10:27 +0800

    fix(scripts-zh-CN): 同步修复中文版BalrogBattle远征setup中level未定义问题

--- gms-server/scripts-zh-CN/event/BalrogBattle.js
+++ gms-server/scripts-zh-CN/event/BalrogBattle.js
@@ -124,7 +124,8 @@ function getEligibleParty(party) {      //selects, from the given party, the tea
 
 function setup(channel) {
     var eim = em.newInstance("Balrog" + channel);
-    eim.setProperty("level", channel);
+    var level = 2;
+    eim.setProperty("level", level);
     eim.setProperty("boss", "0");
     eim.setProperty("canJoin", 1);
 
